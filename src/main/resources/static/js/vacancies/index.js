c5b0d9ed6ed86f341ca4584b3c4df0aa14aa12e2
IgorSq <igor.quintiliano.silva@gmail.com>
2025-03-16 18:31:50 -0300

    Botão X

--- src/main/resources/static/js/vacancies/index.js
+++ src/main/resources/static/js/vacancies/index.js
@@ -1,15 +1,21 @@
 let popUp = document.querySelector(".pop_up");
 let contentPopup = document.querySelector(".content_popup");
+let closeButton = document.getElementById("close-popup");
 
 contentPopup.addEventListener('click', (event) => {
     event.stopPropagation();
 });
 
 function showOccupation() {
-    if(popUp.style.display == "flex") {
+    if (popUp.style.display == "flex") {
         popUp.style.display = "none";
         return;
     }
     
     popUp.style.display = "flex";
 }
+
+// Fecha o popup ao clicar no botão "X"
+closeButton.addEventListener("click", function () {
+    popUp.style.display = "none";
+});
